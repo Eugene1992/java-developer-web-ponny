555c756842cfe24fe0c4e22741847aceaec71263
yaserzhan <ya.serzhan@gmail.com>
2018-12-07 03:27:41 +0200

    added JDBC Homework01(user and product dao)

---
+++ yaroslav/src/main/java/com/cbsystematics/edu/homework01/model/User.java
@@ -0,0 +1,91 @@
+package com.cbsystematics.edu.homework01.model;
+
+
+public class User {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String username;
+    private String password;
+    private String email;
+
+
+    public User(String firstName, String lastName, String username, String password, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public User(Integer id, String firstName, String lastName, String username, String password, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    @Override
+    public String toString() {
+        return "User{"
+                + "id = " + id
+                + ", firstName = " + firstName
+                + ", lastName = " + lastName
+                + ", username = " + username
+                + ", password = " + password
+                + ", email = " + email
+                + '}';
+    }
+}
+
